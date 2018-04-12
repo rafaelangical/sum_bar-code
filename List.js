e4097d732a6c61fdcfbb0d1ab89854e773896de3
rafaelangical <rsnrafael@outlook.com>
2018-04-12 20:25:56 -0300

    Create component list.js

---
+++ List.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default class List extends Component{
+  render(){
+    return{
+      
+    }
+  }
+}
