841be4ceceac410c76b3b7f20387a9073fca8d06
rafaelangical <rsnrafael@outlook.com>
2018-04-12 20:24:22 -0300

    Create menu.js

---
+++ Menu.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default class Menu extends Component{
+  
+  render(){
+    return{
+
+    }
+  }
+}
