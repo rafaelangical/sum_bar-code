364a73f33d95ad581aa7f0d9cfccfc9f0944a2b3
rafaelangical <rsnrafael@outlook.com>
2018-04-12 19:36:35 -0300

    Finished the layout of first page

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 import { Constants, BarCodeScanner, Permissions } from "expo";
 
 export default class App extends Component {
+
+  toSecond() {
+
+  }  
+
   render() {
+ 
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>Sum Code-Bar</Text>
+        <View style={styles.container_top}>
+          <Text style={styles.text}>Sum Code-Bar</Text>
+          <Text style={styles.welcome}>Welcome</Text>
+        </View>
+        <View style={styles.container_bottom}>
+          <Button 
+            onPress={this.toSecond}
+            title="Next"
+            style={styles.button}
+            color = 'red'
+          />
+        </View>
       </View>
     );
   }
@@ -15,11 +32,30 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#333",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
   },
-  text:{
-    fontSize: 20
+  container_top: {
+    height: 300,
+    minWidth: 400,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20
   },
+  container_bottom: {
+    height: 100,
+    minWidth: 400,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 60
+  },
+  welcome: {
+    color: "red",
+    fontSize: 40
+  }
 });
