3b19a472ad4c1b261480411b238aef9e0043083e
rafaelangical <rsnrafael@outlook.com>
2018-04-12 21:32:01 -0300

    Created routes

--- App.js
+++ App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
 import { Image, StyleSheet, Text, View, Alert, Button } from 'react-native';
 import { Constants, BarCodeScanner, Permissions } from "expo";
-import Welcome from './Welcome'
-import Menu from './Menu'
-import List from './List'
+import Welcome from './Welcome';
+import Menu from './Menu';
+import List from './List';
+import { Router, Scene } from 'react-native-router-flux';
+
 
 class App extends Component {
-
   render() {
  
     return (
-      <View style={styles.container}>
-        <Welcome />
-      </View>
+      <Router>
+        <Scene key="root">
+          <Scene
+            key="welcome"
+            component={Welcome}
+            title="Sum_Bar-Code"
+            initial
+          />
+          <Scene 
+            key="menu"
+            component={Menu}
+            title="Menu"
+          />
+          <Scene 
+            key="list"
+            component={List}
+            title="List"
+          />
+        </Scene>
+      </Router>
     );
   }
 }
