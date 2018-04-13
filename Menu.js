3fa913f5c7095ad8cc4b0ddf34eef2d1075cc1b3
rafaelangical <rsnrafael@outlook.com>
2018-04-12 21:22:53 -0300

    Create layout of menu

--- Menu.js
+++ Menu.js
@@ -1,11 +1,54 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
-export default class Menu extends Component{
+class Menu extends Component{
   
   render(){
-    return{
-
-    }
+    return(
+      <View style={styles.container}>
+        <View style={styles.containerTop}>
+          <Text style={{ color: '#456', fontSize: 50}}>Menu</Text>
+        </View>
+        <View style={styles.containerBottom}>
+          <Button 
+            title="Novo"
+          />
+          <Button
+            title="Listar"
+          />
+        </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#999"
+  },
+  containerTop: {
+    padding: 20,
+    minWidth: 400,
+    minHeight: 200,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  containerBottom: {
+    marginTop: 50,
+    flexDirection: "column",
+    width: 200,
+    minHeight: 80,
+    justifyContent: "space-between",
+  }, 
+  button: {
+    fontSize: 10
+  }
+
+});
+
+export default Menu;
